ab350f2be1cb8ac85b960b0ae2dbe04d7de2d600
Adnnoptmz <adnnoptmz@gmail.com>
2026-06-02 15:18:53 +0300

    Add files via upload

---
+++ auth-config.js
@@ -0,0 +1,5 @@
+/*
+  Google OAuth config shared by index.html and account.html.
+*/
+window.GOOGLE_CLIENT_ID = "892007055600-ulari22cmf6puoc2030kffiid80p608r.apps.googleusercontent.com";
+window.GOOGLE_REDIRECT_URI = "https://www.adnnstudio.com";

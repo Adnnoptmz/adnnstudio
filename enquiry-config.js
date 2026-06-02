3264e8b6f53c91f267d6cb3b026d873bd76b904b
Adnnoptmz <adnnoptmz@gmail.com>
2026-06-02 20:10:43 +0300

    Update enquiry-config.js

--- enquiry-config.js
+++ enquiry-config.js
@@ -2,4 +2,4 @@
   Paste the deployed Google Apps Script Web App URL here.
   The website enquiry form will send each submission to that URL.
 */
-window.ADNN_ENQUIRY_ENDPOINT = "https://script.google.com/macros/s/AKfycbyymGudzf83noA8rrDL42RkQDuQu46Ese2TYIyRwEJof46DLFevO4eEVL6xUgsKS9L_/exec";
+window.ADNN_ENQUIRY_ENDPOINT = "https://script.google.com/macros/s/AKfycbzhjeUCm5Q8UXgdalKS7D-YATQm4oghNzDKzL6dui54hvcpSmpCj8D4x3oeNmtxB8WtoQ/exec";

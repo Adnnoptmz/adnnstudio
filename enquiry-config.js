3f64c0f3b471db2cbc3a538a69605bc05887fd06
Adnnoptmz <adnnoptmz@gmail.com>
2026-06-02 19:46:24 +0300

    Add files via upload

---
+++ enquiry-config.js
@@ -0,0 +1,5 @@
+/*
+  Paste the deployed Google Apps Script Web App URL here.
+  The website enquiry form will send each submission to that URL.
+*/
+window.ADNN_ENQUIRY_ENDPOINT = "https://script.google.com/macros/s/AKfycbyymGudzf83noA8rrDL42RkQDuQu46Ese2TYIyRwEJof46DLFevO4eEVL6xUgsKS9L_/exec";

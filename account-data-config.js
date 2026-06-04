37075ef75f6e6c940cdd8c59b875266a58c4e711
Adnnoptmz <adnnoptmz@gmail.com>
2026-06-04 13:15:24 +0300

    Update account-data-config.js

--- account-data-config.js
+++ account-data-config.js
@@ -3,4 +3,4 @@
   This endpoint records successful Google sign-ins and can later serve private
   notifications, tasks, and invoices after server-side token verification.
 */
-window.ADNN_ACCOUNT_ENDPOINT = "https://script.google.com/macros/s/AKfycbyNYpnnZNDHrvY1E2FgJ8jwcqjt0OkfFlYcHlLPhP3birFb3OBDbIGtgS1M6khnmfljLw/exec";
+window.ADNN_ACCOUNT_ENDPOINT = "https://script.google.com/macros/s/AKfycbzuglf4yMKsD_u1zqsgF0ZFUl8ljBZJBkasvgyNDw_96llqow_jVf7_32sscDvQ3Bo4/exec";

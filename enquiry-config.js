a10fa29eccfb589837cd7af7545cab838ee92d04
adhnan.nv <adnnoptmz@gmail.com>
2026-07-01 16:53:03 +0300

    Update enquiry-config.js

--- enquiry-config.js
+++ enquiry-config.js
@@ -1,7 +1,2 @@
-module.exports = function handler(req, res) {
-  res.setHeader("Content-Type", "application/javascript; charset=utf-8");
-  res.setHeader("Cache-Control", "no-store");
-  res.status(200).send(
-    `window.ADNN_ENQUIRY_ENDPOINT = ${JSON.stringify(process.env.ADNN_ENQUIRY_ENDPOINT || "")};\n`
-  );
-};
+*/
+window.ADNN_ENQUIRY_ENDPOINT = "";

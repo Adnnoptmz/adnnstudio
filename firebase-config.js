a1ec4afbc2dabd638a6485c1f975617ddaee8d30
adhnan.nv <adnnoptmz@gmail.com>
2026-07-01 14:14:09 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,25 @@
-window.ADNN_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyChrcjNZdMZ1QehOSDsyWO2jeArnZ1AgCk",
-  authDomain: "adnnstudio-ffdc7.firebaseapp.com",
-  projectId: "adnnstudio-ffdc7",
-  storageBucket: "adnnstudio-ffdc7.firebasestorage.app",
-  messagingSenderId: "525462599967",
-  appId: "1:525462599967:web:51bfcb3cf5f1af629ad82a",
-  measurementId: "G-D6HWQB9V0M"
+function jsonString(value) {
+  return JSON.stringify(String(value || ""));
+}
+
+module.exports = function handler(req, res) {
+  const config = {
+    apiKey: process.env.ADNN_FIREBASE_API_KEY || "",
+    authDomain: process.env.ADNN_FIREBASE_AUTH_DOMAIN || "",
+    projectId: process.env.ADNN_FIREBASE_PROJECT_ID || "",
+    storageBucket: process.env.ADNN_FIREBASE_STORAGE_BUCKET || "",
+    messagingSenderId: process.env.ADNN_FIREBASE_MESSAGING_SENDER_ID || "",
+    appId: process.env.ADNN_FIREBASE_APP_ID || "",
+    measurementId: process.env.ADNN_FIREBASE_MEASUREMENT_ID || ""
+  };
+
+  const ready = Boolean(config.apiKey && config.authDomain && config.projectId && config.appId);
+
+  res.setHeader("Content-Type", "application/javascript; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store");
+  res.status(200).send(
+    ready
+      ? `window.ADNN_FIREBASE_CONFIG = ${JSON.stringify(config)};\n`
+      : "window.ADNN_FIREBASE_CONFIG = null;\n"
+  );
 };
